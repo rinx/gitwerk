de2dede198b4bb4690f143a834ec0a42880c97a2
Rintaro Okamura <rokamura@yahoo-corp.jp>
2020-05-04 13:54:13 +0900

    :sparkles: add sh func
    Signed-off-by: Rintaro Okamura <rokamura@yahoo-corp.jp>

--- src/gitwerk/primitives.clj
+++ src/gitwerk/primitives.clj
@@ -1,10 +1,12 @@
 (ns gitwerk.primitives
   (:require
-    [clojure.pprint :as pprint]))
+    [clojure.pprint :as pprint]
+    [clojure.java.shell :as shell]))
 
 (defn getenv [e]
   (System/getenv e))
 
 (def clj-primitives
   {'pprint pprint/pprint
-   'getenv getenv})
+   'getenv getenv
+   'sh shell/sh})
