3f891de2e839453ff2f7e18f16ad5a120ae151ec
Rintaro Okamura <rokamura@yahoo-corp.jp>
2020-05-15 17:12:50 +0900

    :fire: Remove useless files
    Signed-off-by: Rintaro Okamura <rokamura@yahoo-corp.jp>

--- examples/example2.clj
+++
@@ -1,7 +0,0 @@
-(let [res (semver-auto ctx nil)
-      status (get-in res [:console-out :status])
-      oldv (get-in res [:console-out :old-version])
-      newv (get-in res [:console-out :new-version])]
-  (if (= status :updated)
-    (str "Version updated: " oldv " -> " newv)
-    "Version not updated"))
